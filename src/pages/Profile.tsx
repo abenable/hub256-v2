adc99e9c2c2ca243903e9337a81fc5a2bac9bea9
abenable <ableabenaitwe@gmail.com>
2024-06-04 23:16:48 +0300

    feat: Update user profile page with dynamic user information

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -5,6 +5,16 @@ import userSix from '../images/user/user-06.png';
 import { Link } from 'react-router-dom';
 
 const Profile = () => {
+  const userInfo = {
+    username: 'abenable',
+    email: 'dev@hub256.live',
+    profilePic: 'linkToImage',
+    bgPic: 'linkToImage',
+    role: 'admin',
+    posts: '32',
+    bio: 'new Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque posuere fermentum urna, eu condimentum mauris tempus ut. Donec fermentum blandit aliquet. Etiam dictum dapibus ultricies. Sed vel aliquet libero. Nunc a augue fermentum, pharetra ligula sed, aliquam lacus.',
+  };
+
   return (
     <DefaultLayout>
       <Breadcrumb pageName="Profile" />
@@ -89,13 +99,13 @@ const Profile = () => {
           </div>
           <div className="mt-4">
             <h3 className="mb-1.5 text-2xl font-semibold text-black dark:text-white">
-              Danish Heilium
+              {userInfo.username}{' '}
             </h3>
-            <p className="font-medium">Ui/Ux Designer</p>
-            <div className="mx-auto mt-4.5 mb-5.5 grid max-w-94 grid-cols-3 rounded-md border border-stroke py-2.5 shadow-1 dark:border-strokedark dark:bg-[#37404F]">
+            <p className="font-medium">{userInfo.role}</p>
+            <div className="mx-auto mt-4.5 mb-5.5 grid max-w-94 grid-cols-2 rounded-md border border-stroke py-2.5 shadow-1 dark:border-strokedark dark:bg-[#37404F]">
               <div className="flex flex-col items-center justify-center gap-1 border-r border-stroke px-4 dark:border-strokedark xsm:flex-row">
                 <span className="font-semibold text-black dark:text-white">
-                  259
+                  {userInfo.posts}
                 </span>
                 <span className="text-sm">Posts</span>
               </div>
@@ -105,25 +115,13 @@ const Profile = () => {
                 </span>
                 <span className="text-sm">Followers</span>
               </div>
-              <div className="flex flex-col items-center justify-center gap-1 px-4 xsm:flex-row">
-                <span className="font-semibold text-black dark:text-white">
-                  2K
-                </span>
-                <span className="text-sm">Following</span>
-              </div>
             </div>
 
             <div className="mx-auto max-w-180">
               <h4 className="font-semibold text-black dark:text-white">
                 About Me
               </h4>
-              <p className="mt-4.5">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Pellentesque posuere fermentum urna, eu condimentum mauris
-                tempus ut. Donec fermentum blandit aliquet. Etiam dictum dapibus
-                ultricies. Sed vel aliquet libero. Nunc a augue fermentum,
-                pharetra ligula sed, aliquam lacus.
-              </p>
+              <p className="mt-4.5">{userInfo.bio} </p>
             </div>
 
             <div className="mt-6.5">
