baf0d8e980627170afdfd087ac64481c5a95b64b
abenable <ableabenaitwe@gmail.com>
2024-06-04 21:58:49 +0300

    feat: Add Edit Profile page and route

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,15 @@ import Loader from './common/Loader';
 import PageTitle from './components/PageTitle';
 import SignIn from './pages/Authentication/SignIn';
 import SignUp from './pages/Authentication/SignUp';
-import Calendar from './pages/Calendar';
 import Chart from './pages/Chart';
 import Hub256 from './pages/Dashboard/hub256';
 import FormElements from './pages/Form/FormElements';
 import FormLayout from './pages/Form/FormLayout';
 import Profile from './pages/Profile';
-import Settings from './pages/Settings';
 import Tables from './pages/Tables';
 import Alerts from './pages/UiElements/Alerts';
 import Buttons from './pages/UiElements/Buttons';
+import EditProfile from './pages/EditProfile';
 
 function App() {
   const [loading, setLoading] = useState<boolean>(true);
@@ -46,7 +45,7 @@ function App() {
           path="/profile"
           element={
             <>
-              <PageTitle title="Profile | TailAdmin - Tailwind CSS Admin Dashboard Template" />
+              <PageTitle title="Profile" />
               <Profile />
             </>
           }
@@ -79,11 +78,11 @@ function App() {
           }
         />
         <Route
-          path="/settings"
+          path="/editprofile"
           element={
             <>
-              <PageTitle title="Settings | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-              <Settings />
+              <PageTitle title="Edit Profile" />
+              <EditProfile />
             </>
           }
         />
