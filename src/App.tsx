a9612baae883b13e5023b971885e1bb2c3b1cd6d
abenable <ableabenaitwe@gmail.com>
2024-06-06 00:39:14 +0300

    feat: Update routing paths and page titles for login and registration pages

--- src/App.tsx
+++ src/App.tsx
@@ -51,33 +51,7 @@ function App() {
             </>
           }
         />
-        <Route
-          path="/forms/form-elements"
-          element={
-            <>
-              <PageTitle title="Form Elements | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-              <FormElements />
-            </>
-          }
-        />
-        <Route
-          path="/forms/form-layout"
-          element={
-            <>
-              <PageTitle title="Form Layout | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-              <FormLayout />
-            </>
-          }
-        />
-        <Route
-          path="/tables"
-          element={
-            <>
-              <PageTitle title="Tables | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-              <Tables />
-            </>
-          }
-        />
+
         <Route
           path="/editprofile"
           element={
@@ -97,34 +71,7 @@ function App() {
           }
         />
         <Route
-          path="/chart"
-          element={
-            <>
-              <PageTitle title="Basic Chart | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-              <Chart />
-            </>
-          }
-        />
-        <Route
-          path="/ui/alerts"
-          element={
-            <>
-              <PageTitle title="Alerts | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-              <Alerts />
-            </>
-          }
-        />
-        <Route
-          path="/ui/buttons"
-          element={
-            <>
-              <PageTitle title="Buttons | TailAdmin - Tailwind CSS Admin Dashboard Template" />
-              <Buttons />
-            </>
-          }
-        />
-        <Route
-          path="/auth/signin"
+          path="/login"
           element={
             <>
               <PageTitle title="Login" />
@@ -133,7 +80,7 @@ function App() {
           }
         />
         <Route
-          path="/auth/signup"
+          path="/register"
           element={
             <>
               <PageTitle title="Register" />
