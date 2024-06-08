5734041d0552925b67e09acddc06bb680c6a9bcf
abenable <ableabenaitwe@gmail.com>
2024-06-08 18:34:45 +0300

    refactor: Update util.js to include AWS S3 client configuration

--- src/utils/util.js
+++ src/utils/util.js
@@ -2,6 +2,7 @@ import { fileURLToPath } from 'url';
 import jwt from 'jsonwebtoken';
 import path from 'path';
 import rateLimit from 'express-rate-limit';
+import { S3Client } from '@aws-sdk/client-s3';
 
 export const getDirname = (moduleUrl) => {
   return path.dirname
@@ -16,4 +17,12 @@ export const limiter = rateLimit({
   max: 150000000,
   windowMs: 60 * 60 * 1000,
   message: 'Too many requests from this IP, Please try again in an hour',
+});
+
+export const s3 = new S3Client({
+  credentials: {
+    accessKeyId: awsAccessKeyId,
+    secretAccessKey: awsSecretAccessKey,
+  },
+  region: awsRegion
 });
