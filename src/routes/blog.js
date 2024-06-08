50bb2c33208b690340a20e742e069db5386b730b
abenable <ableabenaitwe@gmail.com>
2024-06-08 19:28:38 +0300

    refactor: Update util.js to include AWS S3 client configuration

--- src/routes/blog.js
+++ src/routes/blog.js
@@ -6,6 +6,7 @@ import logger from '../utils/logger.js';
 import multer from 'multer';
 import { s3 } from '../utils/util.js';
 import { PutObjectCommand, GetObjectCommand, DeleteObjectCommand } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
 
 const router = express.Router();
 
