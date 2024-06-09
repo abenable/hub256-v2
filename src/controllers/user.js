19e8a317940ccaf84cc6641ec409a6f936547353
abenable <ableabenaitwe@gmail.com>
2024-06-09 12:19:13 +0300

    refactor: Update user profile update functionality with image upload support

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -2,8 +2,9 @@ import { ApiError } from './error.js';
 import { UserModel } from '../models/users.js';
 import logger from '../utils/logger.js';
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
-import { DeleteObjectCommand, GetObjectCommand } from '@aws-sdk/client-s3';
+import { DeleteObjectCommand, GetObjectCommand, PutObjectCommand } from '@aws-sdk/client-s3';
 import { s3 } from '../utils/util.js';
+import crypto from 'crypto';
 
 export const allUsers = async (req, res) => {
   try {
@@ -40,6 +41,56 @@ export const userProfile = async (req, res) => {
   } catch (error) {
     logger.error(`An error occurred while fetching user profile: ${error}`);
     new ApiError(500, 'An error occurred while fetching user profile.');
+  }
+};
+
+export const updateProfile = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    logger.info(`Updating user profile with id ${userId}`);
+    const { userName, firstName, lastName, bio, email } = req.body;
+    let image;
+
+    if (req.file) {
+      const imageName = crypto.randomBytes(16).toString('hex');
+      try {
+        await s3.send(new PutObjectCommand({
+          Bucket: process.env.S3_BUCKET_NAME,
+          Key: imageName,
+          Body: req.file.buffer,
+          ContentType: req.file.mimetype,
+        }));
+        image = imageName;
+      } catch (error) {
+        logger.error(`An error occurred while uploading image: ${error}`);
+        return res.status(500).json({ message: 'Image upload failed.' });
+      }
+    }
+
+    const user = await UserModel.findByIdAndUpdate(userId, { userName, firstName, lastName, bio, email, image }, { new: true });
+    logger.info(`User': ${user}`);
+
+    if (!user) {
+      logger.warn(`User with id ${userId} not found`);
+      return res.status(404).json({ message: 'User not found.' });
+    }
+
+    if (req.file && user.image) {
+      try {
+        await s3.send(new DeleteObjectCommand({
+          Bucket: process.env.S3_BUCKET_NAME,
+          Key: user.image
+        }));
+      } catch (error) {
+        logger.error(`An error occurred while deleting old image: ${error}`);
+      }
+    }
+
+    logger.info(`Updated user profile with id ${userId}`);
+    res.json(user);
+  } catch (error) {
+    logger.error(`An error occurred while updating user profile: ${error}`);
+    res.status(500).json({ message: 'An error occurred while updating user profile.' });
   }
 };
 
